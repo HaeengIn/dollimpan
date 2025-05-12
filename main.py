269eb30b0e1a6e9f3bc3f8a53376e9517c4f8350
HaeengIn <haeengin@gmail.com>
2025-05-12 20:08:16 +0900

    fix code

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ dollimpan = [] # 돌림판 리스트 생성
 
 def main(): # 메인 함수
     print("돌리기 / 설정 / 종료")
-    main = input():
-    if main == "돌리기":
+    a = input()
+    if a == "돌리기":
         run() # '돌리기' 입력하면 run 함수 실행
-    if main == "설정":
+    if a == "설정":
         option() # '설정' 입력하면 option 함수 실행
-    if main == "종료":
+    if a == "종료":
         exit() # '종료' 입력하면 exit 함수 실행
     else:
         print("유효하지 않은 입력값입니다.")
@@ -27,13 +27,11 @@ def run():
             pick = random.randint(0, dollimpan_max) # pick에 랜덤 인덱스 값 설정
             print("돌림판의 결과는...")
             print(dollimpan[pick]) # 돌림판 결과 출력
-            again() # 다시 시작 함수 실행
+            # again() # 다시 시작 함수 실행
         if len(dollimpan) == 0: # dollimpan 리스트에 값이 없을 때 실행
             print("돌림판에 아무것도 없습니다.")
 
-def again():
-    # 여기서부터 다시시작 함수
 def option():
-    # 여기서부터 옵션 함수
+    pass # 설정 함수 (추가 기능 구현 예정)
 
 main()
