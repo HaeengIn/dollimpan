6a26bf0429a0df18e3d44d80044aa3a95fd8ad05
HaeengIn <haeengin@gmail.com>
2025-07-01 19:42:48 +0900

    add comment

--- tkinter_main.py
+++ tkinter_main.py
@@ -1,9 +1,13 @@
+# tkinter 임포트
 from tkinter import *
 
+# tkinter 초기 설정
 root = Tk()
-
 root.title('Dol Lim Pan')
 root.geometry('800x600')
 root.resizable(False, False)
 
+
+
+# 종료할 때까지 계속 실행
 root.mainloop()
