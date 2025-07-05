0196706e5e1f303d8f0a9269c74c3a765ff6970e
HaeengIn <haeengin@gmail.com>
2025-07-05 09:27:50 +0900

    code optimize

--- tkinter_main.py
+++ tkinter_main.py
@@ -1,10 +1,10 @@
 # 모듈 임포트
-import tkinter
-import random
-import webbrowser
+import tkinter as tk
+import random as ran
+import webbrowser as web
 
 # tkinter 초기 설정
-root = tkinter.Tk()
+root = tk.Tk()
 root.title('Dol Lim Pan')
 root.geometry('800x600')
 root.resizable(False, False)
@@ -16,7 +16,7 @@ placeholder = '여기에 입력하세요. (최대 4자)'
 class Item:
     def __init__(self, master, value, index):
         self.value = value
-        self.button = tkinter.Button(master, text=value, command=lambda: self.delete(index))
+        self.button = tk.Button(master, text=value, command=lambda: self.delete(index))
         self.button.pack(pady=(10))
 
     def delete(self, index):
@@ -28,7 +28,7 @@ def add_input():
     value = entry.get()
     if value != placeholder and value != '':
         input_list.append(value)
-        entry.delete(0, tkinter.END) # 입력창 비우기
+        entry.delete(0, tk.END) # 입력창 비우기
         render_objects()
 
 # 리스트 객체 렌더링
@@ -45,7 +45,7 @@ def render_objects():
 # 포커스 들어올 때
 def on_focus_in(event):
     if entry.get() == placeholder:
-        entry.delete(0, tkinter.END)
+        entry.delete(0, tk.END)
         entry.config(fg='black')
 
 # 포커스 나갈 때
@@ -74,14 +74,14 @@ def draw_random():
         widget.destroy()
 
     if input_list:
-        winner = random.choice(input_list)
-        winner_btn = tkinter.Button(result_frame, text=f'{winner}')
+        winner = ran.choice(input_list)
+        winner_btn = tk.Button(result_frame, text=f'{winner}')
         winner_btn.pack(pady=(10))
     else:
-        empty_list = tkinter.Label(result_frame, text='리스트가 비어있습니다.', fg='red')
+        empty_list = tk.Label(result_frame, text='리스트가 비어있습니다.', fg='red')
         empty_list.pack(pady=(10))
 
-    reset_btn = tkinter.Button(result_frame, text='재설정', command=reset_all)
+    reset_btn = tk.Button(result_frame, text='재설정', command=reset_all)
     reset_btn.pack(pady=(10))
 
     
@@ -89,7 +89,7 @@ def draw_random():
 def reset_all():
     global input_list
     input_list.clear()
-    entry.delete(0, tkinter.END)
+    entry.delete(0, tk.END)
     entry.insert(0, placeholder)
     entry.config(fg='gray')
     for widget in frame.winfo_children():
@@ -99,12 +99,12 @@ def reset_all():
 
 # 도움말 열기
 def open_help():
-    webbrowser.open_new_tab("https://haeengin.kro.kr/dollimpan_help")
+    web.open_new_tab("https://haeengin.kro.kr/dollimpan_help")
 
 # 입력값 길이 제한
 vcmd = (root.register(validate_length), '%P')
 
-entry = tkinter.Entry(root, fg='gray', validate='key', validatecommand=vcmd, width=25, font=('맑은 고딕', 10)) # 입력창 생성
+entry = tk.Entry(root, fg='gray', validate='key', validatecommand=vcmd, width=25, font=('맑은 고딕', 10)) # 입력창 생성
 entry.insert(0, placeholder) # 플레이스홀더 설정
 entry.pack(pady=(30)) 
 entry.bind("<FocusIn>", on_focus_in) # 포커스 들어올 때
@@ -113,19 +113,19 @@ entry.bind("<Escape>", on_escape) # esc 누르면 포커스 취소
 entry.bind("<Return>", lambda event: add_input()) # 엔터키로 입력값 추가
 root.bind("/", focus_entry) # / 누르면 자동 포커스
 
-button = tkinter.Button(root, text='추가', command=add_input) # 입력값 추가 버튼
+button = tk.Button(root, text='추가', command=add_input) # 입력값 추가 버튼
 button.pack()
 
-frame = tkinter.Frame(root) # 입력값 리스트를 담을 프레임
+frame = tk.Frame(root) # 입력값 리스트를 담을 프레임
 frame.pack(pady=(0))
 
-draw_button = tkinter.Button(root, text='추첨', command=draw_random) # 추첨 버튼
+draw_button = tk.Button(root, text='추첨', command=draw_random) # 추첨 버튼
 draw_button.pack(pady=(5))
 
-result_frame = tkinter.Frame(root) # 추첨 결과를 담을 프레임
+result_frame = tk.Frame(root) # 추첨 결과를 담을 프레임
 result_frame.pack()
 
-help_button = tkinter.Button(root, text='도움말', command=open_help) # 도움말 버튼
+help_button = tk.Button(root, text='도움말', command=open_help) # 도움말 버튼
 help_button.place(relx=1.0, rely=0.0, anchor='ne', x=-10, y=10)  # 우측 상단 정렬
 
 # 종료할 때까지 계속 실행
